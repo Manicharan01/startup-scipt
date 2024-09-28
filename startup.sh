e60066014d4ac6973afea964cb414197db99f369
Manicharan <manicharan150@gmail.com>
2024-09-29 05:29:26 +0530

    My startup script

---
+++ startup.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+REQUIRED_PACKAGES=(
+    "alacritty"
+    "brightnessctl"
+    "chromium"
+    "fd"
+    "fzf"
+    "hypridle"
+    "hyprlock"
+    "hyprpaper"
+    "hyprland"
+    "i3status"
+    "kitty"
+    "libreoffice-still"
+    "mako"
+    "mpd"
+    "mpv"
+    "neofetch"
+    "neovim"
+    "python-pip"
+    "python-pipx"
+    "ripgrep"
+    "stow"
+    "sway"
+    "swaybg"
+    "swaylock"
+    "swayidle"
+    "tmux"
+    "ttf-firacode-nerd"
+    "ttf-font-awesome"
+    "ttf-nerd-fonts-symbols"
+    "ttf-nerd-fonts-symbols-mono"
+    "waybar"
+    "yazi"
+    "yt-dlp"
+    "zsh"
+)
+
+YAY_PKGS = (
+"dracula-gtk-theme"
+"librewolf-bin"
+"spotify-adblock"
+"ulauncher"
+)
+
+sudo pacman -Syu
+
+for package in "${REQUIRED_PACKAGES[@]}"; do
+    echo "Installing $package..."
+    sudo pacman -S -noconfirm "$package"
+done
+
+echo "Installing yay..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Syu
+echo "Yay installed and repository updated"
+
+for yayPackage in "${YAY_PKGS}"; do
+    echo "Installing ${yayPackage}..."
+    yay -S -noconfirm "$yayPackage"
+done
+
+ssh-keygen -t rsa -b 4096 -C "manicharan150@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+
+read -p "Press Enter once you've added the SSH key to GitHub..."
+
+ssh -T git@github.com
+git config --global user.email "manicharan150@gmail.com"
+git config --global user.name "Kollipara Nagavenkatalakshmana Manicharan"
+git config --global init.defaultBranch main
+echo "Git setup complete"
+
+git clone git@github.com:Manicharan01/.dotfiles.git /home/charan/
+
+ISDOT=/home/charan/.dotfiles
+folders=()
+
+if [ -d "$ISDOT" ]; then
+    echo "DOTFILES are there"
+    for folder in "$ISDOT"/*; do
+        if [ -d "$folder" ]; then
+            folders+=("$(basename "$folder")")
+        fi
+    done
+
+    echo "${folders[@]}"
+else
+    echo "No DOTFILES"
+    exit
+fi
+
+if [ "$folders" ]; then
+    for folder in "${folders[@]}"; do
+        if [[ $folder == "i3" || $folder == "iterm2" || $folder == "polybar" || $folder == "rofi" || $folder == "wallpapers" ]]; then
+            continue
+        else
+            echo "Stowing $folder..."
+            #stow "$(basename "$folder")"
+        fi
+    done
+else
+    echo "No Folders"
+fi
+
+echo "Installing banana cursor...."
+pipx install clickgen
+pipx ensurepath
+pnpm i g yarn
+git clone https://github.com/ful1e5/banana-cursor ~/Downloads/
+cd ~/Downloads/banana-cursor
+npx cbmp render.json
+cd ~/Downloads/banana-cursor/themes/
+mkdir -p ~/.local/share/icons
+mv Banana ~/.local/share/icons/
+mv Banana-Blue ~/.local/share/icons/
+mv Banana-Red ~/.local/share/icons/
+mv Banana-Green ~/.local/share/icons/
+echo "Banana cursor installed successfully.."
