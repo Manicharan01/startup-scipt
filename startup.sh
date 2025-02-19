a424aa580d1fa9288a281626a048b323f7245d9f
Manicharan <manicharan150@gmail.com>
2025-02-20 01:25:38 +0530

    New packages

--- startup.sh
+++ startup.sh
@@ -1,75 +1,12 @@
 #!/bin/bash
 
-REQUIRED_PACKAGES=(
-    "alacritty"
-    "brightnessctl"
-    "chromium"
-    "fd"
-    "fakeroot"
-    "fzf"
-    "gcc"
-    "hypridle"
-    "hyprlock"
-    "hyprpaper"
-    "hyprland"
-    "i3status"
-    "kitty"
-    "libreoffice-still"
-    "make"
-    "mako"
-    "mpd"
-    "mpv"
-    "neofetch"
-    "neovim"
-    "python-pip"
-    "python-pipx"
-    "ripgrep"
-    "stow"
-    "sway"
-    "swaybg"
-    "swaylock"
-    "swayidle"
-    "tmux"
-    "ttf-firacode-nerd"
-    "ttf-font-awesome"
-    "ttf-nerd-fonts-symbols"
-    "ttf-nerd-fonts-symbols-mono"
-    "waybar"
-    "yazi"
-    "yt-dlp"
-    "zsh"
-)
+sudo pacman -S --needed - < ./pacman-pkgs.txt
 
-YAY_PKGS=(
-"dracula-gtk-theme"
-"librewolf-bin"
-"spotify-adblock"
-"ulauncher"
-"ttf-meslo-nerd"
-)
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+# curl -fsSL https://get.pnpm.io/install.sh | sh -
 
-sudo pacman -Syu
-
-for package in "${REQUIRED_PACKAGES[@]}"; do
-    echo "Installing $package..."
-    sudo pacman -S --needed - < ./pacman-pkgs.txt
-done
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-
-
-echo "Installing yay..."
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-yay -Syu
-echo "Yay installed and repository updated"
-
-for yayPackage in "${YAY_PKGS[@]}"; do
-    echo "Installing ${yayPackage}..."
-    yay -S --needed - < ./yay-pkgs.txt
-done
+read -p "Press Enter once you've installed the yay AUR helper..."
+yay -S --needed - < ./yay-pkgs.txt
 
 ssh-keygen -t rsa -b 4096 -C "manicharan150@gmail.com"
 eval "$(ssh-agent -s)"
@@ -87,57 +24,13 @@ echo "Git setup complete"
 git clone git@github.com:Manicharan01/.dotfiles.git /home/charan/.dotfiles
 
 ISDOT=/home/charan/.dotfiles
-folders=()
-
-rm -rf ~/.zshrc
 
 if [ -d "$ISDOT" ]; then
     echo "DOTFILES are there"
-    for folder in "$ISDOT"/*; do
-        if [ -d "$folder" ]; then
-            folders+=("$(basename "$folder")")
-        fi
-    done
+    cd ~/.dotfiles
+    stow .
 
-    echo "${folders[@]}"
 else
     echo "No DOTFILES"
     exit
 fi
-
-if [ "$folders" ]; then
-	cd ~/.dotfiles
-    for folder in "${folders[@]}"; do
-        if [[ $folder == "i3" || $folder == "iterm2" || $folder == "polybar" || $folder == "rofi" || $folder == "wallpapers" ]]; then
-            continue
-        else
-            echo "Stowing $folder..."
-            stow "$folder"
-        fi
-    done
-else
-    echo "No Folders"
-fi
-
-source ~/.zshrc
-nvm install node
-pnpm i g npm
-
-echo "Installing banana cursor...."
-pipx install clickgen
-pipx ensurepath
-npm install --global yarn
-git clone https://github.com/ful1e5/banana-cursor ~/Downloads/banana-cursor
-cd ~/Downloads/banana-cursor
-npx cbmp render.json
-npx cbmp render.json
-cd ~/Downloads/banana-cursor/bitmaps/
-mkdir -p ~/.local/share/icons
-mv Banana ~/.local/share/icons/
-mv Banana-Blue ~/.local/share/icons/
-mv Banana-Red ~/.local/share/icons/
-mv Banana-Green ~/.local/share/icons/
-echo "Banana cursor installed successfully.."
-
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
